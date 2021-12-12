82345c36249a0ab36469ad4cb31ceb0e15eab289
howard.salmon <howard.salmon@kakaocorp.com>
2021-12-12 12:41:59 +0900

    Initial Commit

---
+++ Poops/PoopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoopsApp.swift
+//  Poops
+//
+//  Created by howard on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct PoopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
